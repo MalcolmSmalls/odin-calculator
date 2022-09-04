f6f825a462bbbd6180cb6d30edd3bd183b379e12
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-04 14:25:50 -0700

    Store value and display in HTML when clicked

--- js/main.js
+++ js/main.js
@@ -7,5 +7,23 @@ const divide = (num1,num2) => num1 / num2
 
 const operator = (num1,num2) => add(num1,num2)
 
+const numberBtnsAll = document.querySelectorAll('.number')
+const displayedVal = document.querySelector('.display')
 
-console.log(operator(2,2))
+let num1 = 0
+let num2 = 0
+
+
+
+const storeNum = () => {
+    numberBtnsAll.forEach(numbBtn => {
+        numbBtn.addEventListener('click', function(){
+            console.log('hey')
+            num1 = numbBtn.value
+            displayedVal.textContent = `${num1}`
+            console.log(num1)
+        })
+    })
+} 
+
+storeNum()
