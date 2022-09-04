e255cf7723e61edaa016c173ddee3c22f3aad2ce
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-04 13:44:37 -0700

    Add buttons to html

--- js/main.js
+++ js/main.js
@@ -5,3 +5,7 @@ const subtract = (num1,num2) => num1-num2
 const multiply = (num1,num2) => num1*num2
 const divide = (num1,num2) => num1 / num2
 
+const operator = (num1,num2) => add(num1,num2)
+
+
+console.log(operator(2,2))
