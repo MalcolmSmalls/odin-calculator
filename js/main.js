9a55ec7f6433c5a6278d7574492553824c2c855b
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-04 12:09:28 -0700

    Add basic functions, add index and css

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// BASIC FUNCTIONS
+
+const add = (num1,num2) => num1+num2
+const subtract = (num1,num2) => num1-num2
+const multiply = (num1,num2) => num1*num2
+const divide = (num1,num2) => num1 / num2
+
