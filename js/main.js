5d98703ef8560638b730e845020fa695a03724c8
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-06 17:35:37 -0700

    Add functionality to equal button

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const numberBtnsAll = document.querySelectorAll('.nums')
 const displayedVal = document.querySelector('.display')
 const operatorBtns = document.querySelectorAll('.operators')
 const btnsTotal = document.querySelectorAll('button')
+const equalBtn = document.querySelector('.equals')
 let operation
 
 let num = ""
@@ -19,12 +20,33 @@ let total
 let displayOp
 
 
+equalBtn.addEventListener('click', function(){
+    if(num === ''){
+        return
+    }else if(num !== ''){
+        numHolder.push(+num)
+        if(numHolder[1] === "*"){
+            total = numHolder[0]*numHolder[2]
+            num = ""
+        }else if(numHolder[1] === "/"){
+            total = numHolder[0]/numHolder[2]
+            num = ""
+        }else if(numHolder[1] === "+"){
+            total = numHolder[0]+numHolder[2]
+            num = ""
+        }else if(numHolder[1] === "-"){
+            total = numHolder[0]-numHolder[2]
+            num = ""
+        }
+        console.log(total)
+    }
+})
+
 
 numberBtnsAll.forEach(numberBtn => {
     numberBtn.addEventListener('click', function(){
         num += numberBtn.value
         displayedVal.textContent = `${num}`
-        console.log(num)
     })
 })
 
